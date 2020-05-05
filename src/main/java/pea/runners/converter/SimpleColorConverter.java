265de9009641bb235b3009ca686301f012011ce5
RH12503 <rhpizzaboy@gmail.com>
2020-05-05 10:56:04 +0800

    bug fixes

--- src/main/java/pea/runners/converter/SimpleColorConverter.java
+++ src/main/java/pea/runners/converter/SimpleColorConverter.java
@@ -1,13 +1,17 @@
 package pea.runners.converter;
 
-import javafx.scene.paint.Color;
 import pea.util.SimpleColor;
 import picocli.CommandLine;
 
+import java.awt.*;
+import java.lang.reflect.Field;
+
 public class SimpleColorConverter implements CommandLine.ITypeConverter<SimpleColor> {
     @Override
-    public SimpleColor convert(String s) throws Exception {
-        Color color = Color.valueOf(s);
-        return new SimpleColor((float) color.getRed(), (float) color.getGreen(), (float) color.getBlue(), (float) color.getOpacity());
+    public SimpleColor convert(String s) throws ClassNotFoundException, IllegalAccessException, NoSuchFieldException {
+        Color color;
+       Field field = Class.forName("java.awt.Color").getField(s);
+        color = (Color) field.get(null);
+        return new SimpleColor((float) color.getRed(), (float) color.getGreen(), (float) color.getBlue());
     }
 }
